86b5fcc296584b5777fb1f4109d983dc6780d76d
Ivan Vernihora <vernihorai@gmail.com>
2025-08-06 13:32:35 +0200

    finish 05/00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivanvernihora <ivanvernihora@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 12:42:47 by ivanverniho       #+#    #+#             */
+/*   Updated: 2025/08/06 13:28:29 by ivanverniho      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include <ostream>
+
+class Bureaucrat
+{
+    public:
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        Bureaucrat &operator=(const Bureaucrat &copy);
+        ~Bureaucrat(void);
+
+        const std::string &getName(void) const;
+        int getGrade(void) const;
+
+        void incrementGrade(void);
+        void decrementGrade(void);
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+
+    private:
+        Bureaucrat(void);
+        
+        const std::string _name;
+        int _grade;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+#endif
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivanvernihora <ivanvernihora@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/06 11:25:39 by ivanverniho       #+#    #+#             */
+/*   Updated: 2025/08/06 13:30:36 by ivanverniho      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+#include <exception>
+#include <ostream>
+
+int main(void)
+{
+    try
+    {
+        Bureaucrat bureaucrat("John", 150);
+        Bureaucrat bureaucrat2("Jane", 1);
+        Bureaucrat bureaucrat3(bureaucrat);
+
+        //throws exception
+        // Bureaucrat bureaucrat4("Jim", 151);
+        // Bureaucrat bureaucrat5 = bureaucrat;
+
+        std::cout << bureaucrat << std::endl;
+        std::cout << bureaucrat2 << std::endl;
+        std::cout << bureaucrat3 << std::endl;
+        // std::cout << bureaucrat4 << std::endl;
+        // std::cout << bureaucrat5 << std::endl;
+
+        //throws exception
+        // bureaucrat.decrementGrade();
+        bureaucrat3.incrementGrade();
+        //throws exception
+        // bureaucrat2.incrementGrade();
+        std::cout << bureaucrat << std::endl;
+        std::cout << bureaucrat2 << std::endl;
+        std::cout << bureaucrat3 << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+		std::cerr << "* Exception caught: " << e.what() << " *" << std::endl;
+    }
+    
+}
